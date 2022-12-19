ae24481fea8b986f598b02e65246a88c8414ebb2
juma_ <juli@2305>
2022-12-19 02:16:42 -0300

    mentoria e curso

---
+++ src/br/com/desafio/dominio/Main.java
@@ -0,0 +1,28 @@
+package br.com.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[]args){
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("java");
+        curso1.setDescricao("curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("javascrit");
+        curso2.setDescricao("curso javascrit");
+        curso2.setCargaHoraria(8);
+
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("java");
+        mentoria.setDescricao(" mentoria do curso java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
